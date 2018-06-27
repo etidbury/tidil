76435633a3792f0352f958f3dbcd2f58c8b50a6f
etidbury <tidbury@live.com>
2018-06-27 14:45:48 +0100

    add DOCKER_PORT to running docker and exposing docker port

--- util/docker/run.js
+++ util/docker/run.js
@@ -1,41 +1,41 @@
-const chalk=require('chalk');
-const path=require('path');
-module.exports = async ({containerName}) => {
+const chalk = require('chalk');
+const path = require('path');
+module.exports = async ({ containerName }) => {
 
-    require('dotenv').config({path: path.resolve(process.cwd(),'.env')});
+    require('dotenv').config({ path: path.resolve(process.cwd(), '.env') });
 
     await require('exec-sequence').run({
-        'Check Dockerfile exists':{
-            command:"exit 0",
-            exists:"./Dockerfile",
+        'Check Dockerfile exists': {
+            command: "exit 0",
+            exists: "./Dockerfile",
         },
-        'Ensure environment variables are set':{
-            promise:()=>new Promise((resolve, reject)=>{
+        'Ensure environment variables are set': {
+            promise: () => new Promise((resolve, reject) => {
 
                 try {
 
-                    if (process.env.PORT && process.env.PORT.length) {
+                    if (process.env.PORT && process.env.PORT.length && process.env.DOCKER_PORT && process.env.DOCKER_PORT.length) {
                         resolve("All environment variables required are set")
                     } else {
                         reject("Environment variables not set! Please run 'tidil env'");
                     }
-                }catch (err){
+                } catch (err) {
                     reject("Unknown error occurred");
                 }
 
             })
         },
-        'Clean Docker':{
-            promise:()=>Promise.resolve(chalk.yellow("Not yet integrated!"))
+        'Clean Docker': {
+            promise: () => Promise.resolve(chalk.yellow("Not yet integrated!"))
         },
         'Build Docker Container': {
-            command:`docker build -t ${containerName} .`,
+            command: `docker build -t ${containerName} .`,
         },
-        "Run Docker Container":{
-            command:`docker run -it ${containerName} -p ${process.env.PORT}:${process.env.PORT}`,
+        "Run Docker Container": {
+            command: `docker run -it ${containerName} -p ${process.env.PORT}:${process.env.DOCKER_PORT}`,
         }
     })
-        .catch(({cmd, err}) => {
+        .catch(({ cmd, err }) => {
             process.exit(1);
         });
 };
